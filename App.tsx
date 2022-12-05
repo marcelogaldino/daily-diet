59f94ffd3a971f9b82153a2f50cb9930b5610013
marcelogaldino <marcelinhomgo@gmail.com>
2022-12-05 09:59:45 -0300

    feat: status bar

--- App.tsx
+++ App.tsx
@@ -5,12 +5,18 @@ import { Home } from '@screens/Home';
 import { Loading } from '@components/Loading';
 
 import theme from './src/theme';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold })
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle='dark-content'
+        backgroundColor="transparent"
+        translucent
+      />
       {!fontsLoaded ? <Home /> : <Loading />}
     </ThemeProvider>
   );
