72d6ea5db5be1124276ad59b304683e0ce42e792
marcelogaldino <marcelinhomgo@gmail.com>
2022-12-05 09:34:09 -0300

    feat: loading component

--- App.tsx
+++ App.tsx
@@ -1,15 +1,17 @@
 import { ThemeProvider } from 'styled-components'
 import { useFonts, NunitoSans_400Regular, NunitoSans_700Bold } from '@expo-google-fonts/nunito-sans'
-import { Home } from './src/screens/Home';
+
+import { Home } from '@screens/Home';
+import { Loading } from '@components/Loading';
+
 import theme from './src/theme';
-import { ActivityIndicator } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold })
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Home /> : <ActivityIndicator />}
+      {!fontsLoaded ? <Home /> : <Loading />}
     </ThemeProvider>
   );
 }
