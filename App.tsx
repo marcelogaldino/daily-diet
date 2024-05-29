bcc9d6ea8ece3b37b53efcddcbf4dcde918d292c
Marcelo Galdino <marcelo.oliveira@gazin.com.br>
2024-05-29 14:09:07 -0300

    feat: Home screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StatusBar, Text, View } from "react-native";
+import {
+  useFonts,
+  Nunito_400Regular,
+  Nunito_700Bold,
+} from "@expo-google-fonts/nunito";
+import { Home } from "@screens/Home";
+import { ThemeProvider } from "styled-components/native";
+import theme from "@theme/index";
+import { Loading } from "@components/Loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_400Regular,
+    Nunito_700Bold,
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="dark-content"
+        translucent
+        backgroundColor="transparent"
+      />
+      {fontsLoaded ? <Home /> : <Loading />}
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles";
+import { HeaderProfile } from "@components/HeaderProfile";
+
+export function Home() {
+  return (
+    <Container>
+      <HeaderProfile />
+    </Container>
+  );
+}
